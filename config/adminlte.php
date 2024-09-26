70832a19afd2512f1b411e128b76b864a4bc6226
yujirotakubo <167697620+yujirotakubo@users.noreply.github.com>
2024-09-27 07:20:21 +0900

    サイドバーAPP名変更

--- config/adminlte.php
+++ config/adminlte.php
@@ -63,12 +63,12 @@ return [
     |
     */
 
-    'logo' => '<b>備蓄品管理</b>',
+    'logo' => '<b>StockMaster Nexus</b>',
     'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
-    'logo_img_alt' => '備蓄品管理',
+    'logo_img_alt' => 'StockMaster Nexus',
 
     /*
     |--------------------------------------------------------------------------
